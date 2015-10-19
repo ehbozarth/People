ab7f251a8dbe6d0d48062fac307fb9e6631aaf10
earlbozarth <ehbbozarth@gmail.com>
2015-10-19 13:46:26 -0400

    Homework 3.1 People with Countries

---
+++ src/People.java
@@ -0,0 +1,79 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+/**
+ * Created by earlbozarth on 10/19/15.
+ */
+public class People {
+    public static void main(String[] args) {
+        String filename = "people.csv";
+        String peopleContent = readFile(filename);
+        HashMap<String, ArrayList<Person>> peopleMap = new HashMap<>();
+        String [] lines = peopleContent.split("\n");
+
+
+        for(String line : lines){
+            String [] columns = line.split(",");
+            String id = columns[0];
+            String firstName = columns[1];
+            String lastName = columns[2];
+            String email = columns[3];
+            String country = columns[4];
+            String ipAddress = columns[5];
+
+            Person person1 = new Person(id, firstName, lastName, email, country,ipAddress);
+
+            String countryName = String.valueOf(columns[4]);
+            ArrayList<Person> pCountry = peopleMap.get(countryName);
+
+            if(pCountry == null){
+                pCountry = new ArrayList<>();
+                pCountry.add(person1);
+                peopleMap.put(countryName, pCountry);
+            }//End of if Statement
+            else{
+                pCountry.add(person1);
+            }
+
+            Collections.sort(pCountry);
+            //Sorting the collection of pCountry List by Last name
+
+        }//End of For Loop
+
+        System.out.println(peopleMap.toString());
+
+    }//End of Main Method
+
+    static String readFile(String fileName){
+        File f = new File(fileName);
+        try{
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char [] fileContent = new char[fileSize];
+            fr.read(fileContent);
+            return new String(fileContent);
+        }//End of try
+        catch (Exception e){
+            System.out.println("Error...Error...Error");
+            return null;
+        }//End of catch
+    }//end of readFile
+
+    static void writeFile(String fileName,String fileContent){
+        File f = new File(fileName);
+        try{
+            FileWriter fw = new FileWriter(f);
+            fw.write(fileContent);
+            fw.close();
+        }//End of Try
+        catch(Exception e){
+            System.out.println("Error...Error...Error");
+        }//End of Catch
+    }//End of wrtieFile
+
+
+}//End of People Class
---
+++ src/Person.java
@@ -0,0 +1,33 @@
+/**
+ * Created by earlbozarth on 10/19/15.
+ */
+public class Person implements Comparable{
+    String id;
+    String firstName;
+    String lastName;
+    String email;
+    String country;
+    String ipAddress;
+
+    public Person(String id, String firstName, String lastName, String email, String country, String ipAddress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.country = country;
+        this.ipAddress = ipAddress;
+    }//End of Constructor
+
+    @Override
+    public int compareTo(Object o) {
+        Person p = (Person) o;
+        int result = lastName.compareTo(p.lastName);
+        return result;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s %s is from %s %s \n", firstName, lastName, country, ipAddress);
+    }
+
+}//End of Person Class
